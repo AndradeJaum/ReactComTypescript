0b1587b7faab39694a2092c0b4c3ce20170131d3
JoãoVitorAndrade <andradejaum@gmail.com>
2022-03-28 11:37:14 -0300

    Refatorado

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,27 +1,22 @@
-import { useState } from 'react';
+import { Itarefa } from '../../Types/tarefa';
 import Item from './Item';
 import style from './Lista.module.scss';
 
-function Lista() {
-  const [tarefas, setTarefas] = useState([{
-    tarefa: 'React',
-    tempo: '02:00:00'
-  }, {
-    tarefa: 'Javascript',
-    tempo: '01:00:00'
-  }, {
-    tarefa: "Typescript",
-    tempo: "03:00:00"
-  }]); 
+interface Props {
+  tarefas: Itarefa[],
+  selecionaTarefa: (tarefaSelecionad: Itarefa) => void
+}
+
+function Lista({ tarefas, selecionaTarefa }: Props) {
+
   return (
     <aside className={style.listaTarefas}>
-      <h2 onClick={() => {
-        setTarefas([...tarefas, { tarefa: "Estudar estado", tempo: "05:00:00"}]);
-      }}> Estudos do dia </h2>
+      <h2> Estudos do dia </h2>
       <ul>
-        {tarefas.map((item, index) => (
+        {tarefas.map(item => (
           <Item
-            key={index}
+            selecionaTarefa={selecionaTarefa}
+            key={item.id}
             {...item}
           />
         ))}
